21599bf33f635feb83f0c1d23b5ad715f9180384
Carlos Perez <ca.perez.barr@gmail.com>
2021-08-01 23:07:31 -0500

    Troubleshooting: HTML Rendering

--- generateHTML.js
+++ generateHTML.js
@@ -30,16 +30,15 @@ function generateHTML(data) {
 
     //Render Manager HTML
     const renderManager = (data) => {
-        return `
-    <div class="card" style="width: 18rem;">
+        let renderedManagerCards = `<div class="card" style="width: 18rem;">
     <div class="card-body">
     <h5 class="card-title">Manager</h5>
-    <h6 class="card-subtitle mb-2 text-muted">${data.name}, ID: ${data.id}</h6>
+    <h6 class="card-subtitle mb-2 text-muted">${data.fullName}, ID: ${data.id}</h6>
     <p class="card-text">Office Number: ${data.officeNumber}</p>
     <a href="${data.email}" class="card-link">Email: ${data.email}</a>
     </div>
-    </div>
-    `
+    </div>`
+        return renderedManagerCards;
     }
 
     //Render Engineer HTML
@@ -83,20 +82,20 @@ function generateHTML(data) {
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <title>Staff Directory</title>
   </head>
   <body>
-    ${renderManager(roleManager)}
-    ${renderEngineer(roleEngineer)}
-    ${renderIntern(roleIntern)}
+    
+   ${roleManager.forEach(Manager => renderManager(Manager))}
 
 
+    
+    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-  </body>
-</html>
-`
+    </body>
+    </html>
+    `
 };
 
 // let Leah = new Manager('Leah', '1', 'leah@gmail.com', 'A37')
@@ -106,4 +105,6 @@ function generateHTML(data) {
 
 // generateHTML(data);
 
+// ${renderEngineer(roleEngineer)}
+// ${renderIntern(roleIntern)}
 module.exports = generateHTML;
