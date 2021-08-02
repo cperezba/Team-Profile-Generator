aa78b035abfb6b29f2ebadd65957091faf6c33b5
Carlos Perez <ca.perez.barr@gmail.com>
2021-08-01 20:59:51 -0500

    Update: Managing HTML Rendering

---
+++ generateHTML.js
@@ -0,0 +1,109 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+
+
+function generateHTML(data) {
+    // console.log(data);
+    let roleManager = [];
+    let roleEngineer = [];
+    let roleIntern = [];
+
+    data.forEach(emp => {
+
+        if (emp.getRole() === 'Manager') {
+            roleManager.push(emp);
+        } else if (emp.getRole() === 'Engineer') {
+            roleEngineer.push(emp)
+        } else {
+            roleIntern.push(emp);
+        }
+
+        // console.log(roleManager);
+        // console.log(roleEngineer);
+        // console.log(roleIntern);
+    })
+
+
+
+    //Render Manager HTML
+    const renderManager = (data) => {
+        return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Manager</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${data.name}, ID: ${data.id}</h6>
+    <p class="card-text">Office Number: ${data.officeNumber}</p>
+    <a href="${data.email}" class="card-link">Email: ${data.email}</a>
+    </div>
+    </div>
+    `
+    }
+
+    //Render Engineer HTML
+    const renderEngineer = (data) => {
+        return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Engineer</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${data.name}, ID: ${data.id}</h6>
+    <p class="card-text">Github: ${data.gitHub}</p>
+    <a href="${data.email}" class="card-link">Email: ${data.email}</a>
+    </div>
+    </div>
+    `
+    }
+
+    //Render Intern HTML
+    const renderIntern = (data) => {
+        return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">Intern</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${data.name}, ID: ${data.id}</h6>
+    <p class="card-text">Contributing School: ${data.school}</p>
+    <a href="${data.email}" class="card-link">Email: ${data.email}</a>
+    </div>
+    </div>
+    `
+    }
+
+
+
+    //Render Full HTML
+    return `
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    ${renderManager(roleManager)}
+    ${renderEngineer(roleEngineer)}
+    ${renderIntern(roleIntern)}
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
+`
+};
+
+// let Leah = new Manager('Leah', '1', 'leah@gmail.com', 'A37')
+// let Christian = new Engineer('Christian', '3', 'christian@gmail.com', 'chery5')
+// let Carlos = new Intern('Carlos', '7', 'me@me.com', 'Carvin');
+// let data = [Leah, Christian, Carlos];
+
+// generateHTML(data);
+
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,2 +1,218 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require("./generateHTML");
 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+
+//User Questions:
+// const questions = [{
+//   type: 'input',
+//   name: 'departmentStaff',
+//   message: `What is the directory of this staff?`,
+// },
+// {
+//   type: 'input',
+//   name: 'employeeName',
+//   message: `What is employee's name?`,
+// },
+// {
+//   type: 'input',
+//   name: 'employeeID',
+//   message: `What is employee's id?`,
+// },
+// {
+//   type: 'input',
+//   name: 'employeeEmail',
+//   message: `What is employee's email?`,
+// },
+// {
+//   type: 'input',
+//   name: 'managerOfficeNumber',
+//   message: `What is employee's office number?`,
+// },
+// {
+//   type: 'input',
+//   name: 'engineerGithub',
+//   message: `What is employee's Github?`,
+// },
+// {
+//   type: 'input',
+//   name: 'internSchool',
+//   message: `What is employee's school?`,
+// },
+// {
+//   type: 'list',
+//   message: `What is the employee's position?`,
+//   name: 'staffPosition',
+//   choices: ['Manager', 'Engineer', 'Intern']
+// },
+// {
+//   when: function (user) {
+//     return user.staffPosition === 'Manager';
+//   },
+//   type: 'input',
+//   name: 'test1',
+//   message: 'Testing Manager',
+// },
+// {
+//   when: function (user) {
+//     return user.staffPosition === 'Engineer';
+//   },
+//   type: 'input',
+//   name: 'test2',
+//   message: 'Testing Engineer',
+// },
+// {
+//   when: function (user) {
+//     return user.staffPosition === 'Intern';
+//   },
+//   type: 'input',
+//   name: 'test3',
+//   message: 'Testing Intern',
+// }];
+
+
+
+// function init() {
+//   inquirer.prompt(questions)
+//     .then((responses) => {
+//       const filename = `${responses.departmentStaff.toLowerCase().split(' ').join('')}.json`;
+//       // TODO: Create a function to write README file
+//       console.log(responses);
+//       fs.writeFile(`index.html`, generateHTML(responses), (err) =>
+//         err ? console.log(err) : console.log('Success!')
+//       );
+//     });
+// }
+
+// //Initialize application
+// init();
+
+
+
+//Main Questions Menu
+let team = []; //*
+const mainQuery = () => {
+  inquirer.prompt({
+    type: 'list',
+    message: `What is the employee's position?`,
+    name: 'staffPosition',
+    choices: ['Manager', 'Engineer', 'Intern', 'Done']
+  })
+
+    .then(result => {
+      switch (result.staffPosition) {
+        case 'Manager':
+          managerData()
+          break;
+        case 'Engineer':
+          engineerData()
+          break;
+        case 'Intern':
+          internData()
+          break;
+        case 'Done':
+          console.log(team)
+          fs.writeFile(`team.html`, generateHTML(team), (err) =>
+            err ? console.log(err) : console.log('Success!'))
+          break;
+      }
+    })
+}
+
+
+//Manager Questions
+const managerData = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'employeeName',
+    message: `What is employee's name?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeID',
+    message: `What is employee's id?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeEmail',
+    message: `What is employee's email?`,
+  },
+  {
+    type: 'input',
+    name: 'managerOfficeNumber',
+    message: `What is employee's office number?`,
+  }])
+    .then(result => {
+      let manager = new Manager(result.employeeName, result.employeeID, result.employeeEmail, result.managerOfficeNumber);
+      team.push(manager)
+      mainQuery()
+    })
+}
+
+
+//Engineer Questions
+const engineerData = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'employeeName',
+    message: `What is employee's name?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeID',
+    message: `What is employee's id?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeEmail',
+    message: `What is employee's email?`,
+  },
+  {
+    type: 'input',
+    name: 'engineerGithub',
+    message: `What is employee's Github?`,
+  }])
+    .then(result => {
+      let engineer = new Engineer(result.employeeName, result.employeeID, result.employeeEmail, result.managerOfficeNumber);
+      team.push(engineer)
+      mainQuery()
+    })
+}
+
+//Intern Questions
+const internData = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'employeeName',
+    message: `What is employee's name?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeID',
+    message: `What is employee's id?`,
+  },
+  {
+    type: 'input',
+    name: 'employeeEmail',
+    message: `What is employee's email?`,
+  },
+  {
+    type: 'input',
+    name: 'internSchool',
+    message: `What is employee's school?`,
+  }])
+    .then(result => {
+      let intern = new Intern(result.employeeName, result.employeeID, result.employeeEmail, result.managerOfficeNumber);
+      team.push(intern)
+      mainQuery()
+    })
+}
+
+
+
+mainQuery();
